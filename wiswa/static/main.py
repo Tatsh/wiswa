09b1a88a3d436c5a4a03bb8b39ec8872f9905cb9
Andrew Udvare <audvare@gmail.com>
2025-04-25 19:22:44 -0400

    Fixes

--- wiswa/static/main.py
+++ wiswa/static/main.py
@@ -1,16 +1,17 @@
 """Main script."""
 from __future__ import annotations
 
-import logging
-
 import click
+
+from .utils import setup_logging
 
 __all__ = ('main',)
 
 
 @click.command(context_settings={'help_option_names': ('-h', '--help')})
+@click.option('--force-color', help='Force enable colour log output.', is_flag=True)
+@click.option('--no-color', help='Disable colour log output.', is_flag=True)
 @click.option('-d', '--debug', help='Enable debug level logging.', is_flag=True)
-def main(*, debug: bool = False) -> None:
-    logging.basicConfig(format='%(levelname)s:%(module)s:%(lineno)d:%(funcName)s: %(message)s',
-                        level=logging.DEBUG if debug else logging.WARNING)
+def main(*, debug: bool = False, force_color: bool = False, no_color: bool = False) -> None:
+    setup_logging(force_color=force_color, debug=debug, no_color=no_color)
     click.echo('Do something here.')
