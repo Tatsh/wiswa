570d6d58d7026146ad5ff5361bdb8067ef819e0c
Andrew Udvare <audvare@gmail.com>
2025-05-18 01:31:11 -0400

    wiswa/static/main: logging

--- wiswa/static/main.py
+++ wiswa/static/main.py
@@ -9,9 +9,7 @@ __all__ = ('main',)
 
 
 @click.command(context_settings={'help_option_names': ('-h', '--help')})
-@click.option('--force-color', help='Force enable colour log output.', is_flag=True)
-@click.option('--no-color', help='Disable colour log output.', is_flag=True)
 @click.option('-d', '--debug', help='Enable debug level logging.', is_flag=True)
 def main(*, debug: bool = False, force_color: bool = False, no_color: bool = False) -> None:
-    setup_logging(force_color=force_color, debug=debug, no_color=no_color)
+    setup_logging(debug=debug)
     click.echo('Do something here.')
