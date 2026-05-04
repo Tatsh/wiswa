88455fdd755ebe4b91f3b9dbbc41ec2506acdddb
Andrew Udvare <audvare@gmail.com>
2026-05-04 18:05:18 -0400

    docs.conf: update intersphinx mapping
    Signed-off-by: Andrew Udvare <audvare@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -76,8 +76,13 @@ html_theme_options = {
     'toc_title_is_page_title': True
 }
 intersphinx_mapping = {
-    'jinja2': ('https://jinja.palletsprojects.com/en/latest', None),
-    'niquests': ('https://niquests.readthedocs.io/en/latest', None),
-    'niquests-cache': ('https://niquests-cache.readthedocs.io/en/latest', None),
-    'python': ('https://docs.python.org/3', None)
+    'bascom': ('https://bascom.readthedocs.io/en/latest/', None),
+    'click': ('https://click.palletsprojects.com/en/latest/', None),
+    'jinja2': ('https://jinja.palletsprojects.com/en/latest/', None),
+    'keyring': ('https://keyring.readthedocs.io/en/latest/', None),
+    'niquests': ('https://niquests.readthedocs.io/en/latest/', None),
+    'platformdirs': ('https://platformdirs.readthedocs.io/en/latest/', None),
+    'python': ('https://docs.python.org/3', None),
+    'tomlkit': ('https://tomlkit.readthedocs.io/en/latest/', None),
+    'typing-extensions': ('https://typing-extensions.readthedocs.io/en/latest/', None)
 }
